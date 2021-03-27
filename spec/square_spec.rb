9e7e4bf1f01a3709deb6880f1f3403f9612a75ed
Magmusacy <xpfk.098@gmail.com>
2021-03-27 19:03:15 +0100

    Create square class with update_piece method

---
+++ spec/square_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative '../lib/square'
+
+describe Square do
+  describe '#update_piece' do
+    context 'when given instance of a piece as an argument' do
+      subject(:square_piece) { described_class.new([:b, 2]) }
+      let(:piece) { double('piece') }
+      it 'changes @piece attribute to that instance' do
+        square_piece.update_piece(piece)
+        updated_piece = square_piece.piece
+        expect(updated_piece).to be(piece)
+      end
+    end
+  end
+end
