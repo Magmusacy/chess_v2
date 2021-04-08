7475eed7eff07eddae458775b58a5b31d17a2d4d
Magmusacy <xpfk.098@gmail.com>
2021-04-08 16:35:35 +0200

    Change method names to singular form

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -5,12 +5,12 @@ require_relative 'piece'
 # Contains logic for Pawn movement
 class Pawn < Piece
   def legal_moves(board)
-    y_axis_moves(board, y_axis_shift)
-    diagonal_moves(board, y_axis_shift)
-    en_passant(board, y_axis_shift)
+    y_axis_move(board, y_axis_shift)
+    diagonal_move(board, y_axis_shift)
+    en_passant_move(board, y_axis_shift)
   end
 
-  def y_axis_moves(board, y_shift)
+  def y_axis_move(board, y_shift) # zmien w testach z moves na move w tych metodach jo
     ary = []
     new_pos = clone_position(y: y_shift)
     ary << board.get_square(new_pos) unless board.square_taken?(new_pos)
@@ -21,7 +21,7 @@ class Pawn < Piece
     ary
   end
 
-  def diagonal_moves(board, y_shift)
+  def diagonal_move(board, y_shift)
     ary = []
     new_positions = [clone_position(x: -1, y: y_shift),
                      clone_position(x: 1, y: y_shift)]
@@ -33,8 +33,6 @@ class Pawn < Piece
     end
     ary
   end
-
-  def en_passant(board, y_shift); end
 
   def y_axis_shift
     color == :white ? 1 : -1
