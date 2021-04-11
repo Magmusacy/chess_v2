656eb98b5416454f104ce2580d9fb5a04aa6a8c3
Magmusacy <xpfk.098@gmail.com>
2021-04-11 20:23:03 +0200

    Add test for #update_piece default argument

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -14,5 +14,15 @@ describe Square do
         expect(updated_piece).to be(piece)
       end
     end
+
+    context 'when given no argument' do
+      subject(:default_piece) { described_class.new([1, 1]) }
+
+      it 'changes @piece attribute to \'   \'' do
+        default_piece.update_piece
+        updated_piece = default_piece.piece
+        expect(updated_piece).to eq('   ')
+      end
+    end
   end
 end
