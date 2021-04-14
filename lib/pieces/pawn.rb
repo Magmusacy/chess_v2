876a48005a0b16bfede8ff644d9f7ea7960c5630
Magmusacy <xpfk.098@gmail.com>
2021-04-14 18:23:27 +0200

    Add EnPassantMove module

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,35 +1,36 @@
 # frozen_string_literal: true
 
 require_relative 'piece'
+require_relative '../modules/en_passant_move'
 
 # Contains logic for Pawn movement
 class Pawn < Piece
+  include EnPassantMove
   def legal_moves(board)
-    y_axis_move(board, y_axis_shift)
-    diagonal_move(board, y_axis_shift)
-    en_passant_move(board, y_axis_shift)
+    y_axis_move(board)
+    diagonal_move(board)
+    en_passant_move(board)
   end
 
-  def y_axis_move(board, y_shift) # zmien w testach z moves na move w tych metodach jo
+  def y_axis_move(board, y_shift = y_axis_shift)
     ary = []
     new_pos = clone_position(y: y_shift)
     ary << board.get_square(new_pos) unless board.square_taken?(new_pos)
     if [2, 7].include?(location.position[:y]) && !ary.empty?
-      new_pos[:y] += 1
+      new_pos[:y] += y_shift
       ary << board.get_square(new_pos) unless board.square_taken?(new_pos)
     end
     ary
   end
 
-  def diagonal_move(board, y_shift)
+  def diagonal_move(board, y_shift = y_axis_shift)
     ary = []
-    new_positions = [clone_position(x: -1, y: y_shift),
-                     clone_position(x: 1, y: y_shift)]
+    new_positions = [clone_position(x: -1, y: y_shift), clone_position(x: 1, y: y_shift)]
     2.times do |i|
-      if board.square_taken?(new_positions[i])
-        new_sqr = board.get_square(new_positions[i])
-        ary << new_sqr unless new_sqr.piece.color == color
-      end
+      next unless board.square_taken?(new_positions[i])
+
+      new_sqr = board.get_square(new_positions[i])
+      ary << new_sqr unless new_sqr.piece.color == color
     end
     ary
   end
