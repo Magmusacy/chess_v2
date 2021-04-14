47b4bb32f53a98bdf80d7821200dfd4fd08684f5
Magmusacy <xpfk.098@gmail.com>
2021-04-14 18:18:06 +0200

    Change order of attributes when creating Pawn objects

--- spec/pieces/pawn_spec.rb
+++ spec/pieces/pawn_spec.rb
@@ -2,7 +2,7 @@
 
 require_relative 'shared_piece_spec'
 require_relative '../../lib/pieces/pawn'
-# require_relative '../../lib/pieces/board'
+# require_relative '../../lib/board'
 require_relative '../../lib/pieces/square'
 
 describe Pawn do
@@ -20,7 +20,7 @@ describe Pawn do
 
   describe '#y_axis_shift' do
     context 'when @color is :black' do
-      subject(:pawn) { described_class.new(:black, nil) }
+      subject(:pawn) { described_class.new(nil, :black) }
 
       it 'returns -1' do
         result = pawn.y_axis_shift
@@ -29,7 +29,7 @@ describe Pawn do
     end
 
     context 'when @color is :white' do
-      subject(:pawn) { described_class.new(:white, nil) }
+      subject(:pawn) { described_class.new(nil, :white) }
 
       it 'returns 1' do
         result = pawn.y_axis_shift
@@ -41,9 +41,9 @@ describe Pawn do
   describe '#y_axis_move' do
     context 'when given Pawn object with square position: { x: 2, y: 2 }' do
       let(:start_square) { instance_double(Square, position: { x: 2, y: 2 }) }
-      subject(:wht_pawn) { described_class.new(:white, '♙', start_square) }
+      subject(:wht_pawn) { described_class.new(start_square, :white) }
 
-      context 'when square with position: { x: 2, y: 3 } and { x: 2, y: 4 } are empty' do
+      context 'when squares { x: 2, y: 3 } and { x: 2, y: 4 } are empty' do
         let(:board_y3_y4) { double('board', board: position_array) }
         before do
           board_y3_y4.board.map! do |sqr|
@@ -71,7 +71,7 @@ describe Pawn do
 
         before do
           board_y4.board.map! do |sqr|
-            if sqr[2, 4]
+            if sqr == [2, 4]
               instance_double(Square, position: { x: sqr.first, y: sqr.last }, piece: enemy_piece)
             else
               instance_double(Square, position: { x: sqr.first, y: sqr.last }, piece: '   ')
@@ -99,7 +99,7 @@ describe Pawn do
 
         before do
           board_zero.board.map! do |sqr|
-            if sqr[2, 3]
+            if sqr == [2, 3]
               instance_double(Square, position: { x: sqr.first, y: sqr.last }, piece: enemy_piece)
             else
               instance_double(Square, position: { x: sqr.first, y: sqr.last }, piece: ' ')
@@ -119,7 +119,7 @@ describe Pawn do
 
     context 'when given white Pawn object with square position: { x: 2, y: 5 }' do
       let(:start_square) { instance_double(Square, position: { x: 2, y: 5 }) }
-      subject(:wht_pawn) { described_class.new(:white, '♙', start_square) }
+      subject(:wht_pawn) { described_class.new(start_square, :white) }
 
       context 'when square: { x: 2, y: 6 } has default @piece' do
         let(:board_y6) { double('board', board: position_array) }
@@ -147,7 +147,7 @@ describe Pawn do
   describe '#diagonal_move' do
     context 'when given white Pawn object with square position: { x: 2, y: 2 }' do
       let(:start_square) { instance_double(Square, position: { x: 2, y: 2 }) }
-      subject(:wht_pawn) { described_class.new(:white, '♙', start_square) }
+      subject(:wht_pawn) { described_class.new(start_square, :white) }
 
       context 'when two white Pieces are on squares { x: 1, y: 3 }, { x: 3, y: 3 }' do
         let(:my_piece) { instance_double(Piece, color: :white) }
@@ -242,7 +242,7 @@ describe Pawn do
 
     context 'when given black Pawn on square: { x: 1, y: 7 }' do
       let(:start_square) { double('Square', position: { x: 1, y: 7 }) }
-      let(:blk_pawn) { described_class.new(:black, '♟︎', start_square) }
+      let(:blk_pawn) { described_class.new(start_square, :black) }
 
       context 'when given white enemy Piece on square: { x: 2, y: 6 }' do
         let(:enemy_piece) { double('Piece', color: :white) }
@@ -274,6 +274,8 @@ describe Pawn do
     end
   end
 
+  # refator those tests because they are toooooo long
+
   describe '#en_passant_move' do
     context 'when given white Pawn { x: 5, y: 5 } and black Pawn { x: 6, y: 5 } that has just moved from { x: 6, y: 7 }' do
       let(:en_passant_board) { double('board', board: position_array) }
@@ -281,7 +283,7 @@ describe Pawn do
       let(:blk_pawn_sqr) { instance_double(Square, position: { x: 6, y: 5 }, piece: blk_pawn) }
       let(:prev_blk_pawn_sqr) { instance_double(Square, position: { x: 6, y: 7 }) }
       let(:start_square) { instance_double(Square, position: { x: 5, y: 5 }) }
-      subject(:wht_pawn) { described_class.new(:white, '♙', start_square) }
+      subject(:wht_pawn) { described_class.new(start_square, :white) }
 
       before do
         en_passant_board.board.map! do |sqr|
@@ -314,7 +316,7 @@ describe Pawn do
       let(:blk_pawn_sqr) { instance_double(Square, position: { x: 4, y: 5 }, piece: blk_pawn) }
       let(:prev_blk_pawn_sqr) { instance_double(Square, position: { x: 4, y: 7 }) }
       let(:start_square) { instance_double(Square, position: { x: 5, y: 5 }) }
-      subject(:wht_pawn) { described_class.new(:white, '♙', start_square) }
+      subject(:wht_pawn) { described_class.new(start_square, :white) }
 
       before do
         en_passant_board.board.map! do |sqr|
@@ -347,7 +349,7 @@ describe Pawn do
       let(:wht_pawn_sqr) { instance_double(Square, position: { x: 2, y: 4 }, piece: wht_pawn) }
       let(:prev_wht_pawn_sqr) { instance_double(Square, position: { x: 2, y: 2 }) }
       let(:start_square) { instance_double(Square, position: { x: 1, y: 4 }) }
-      subject(:blk_pawn) { described_class.new(:black, '♟︎', start_square) }
+      subject(:blk_pawn) { described_class.new(start_square, :black) }
 
       before do
         en_passant_board.board.map! do |sqr|
@@ -380,7 +382,7 @@ describe Pawn do
       let(:blk_pawn_sqr) { instance_double(Square, position: { x: 6, y: 5 }, piece: blk_pawn) }
       let(:prev_blk_pawn_sqr) { instance_double(Square, position: { x: 6, y: 6 }) }
       let(:start_square) { instance_double(Square, position: { x: 5, y: 5 }) }
-      subject(:wht_pawn) { described_class.new(:white, '♙', start_square) }
+      subject(:wht_pawn) { described_class.new(start_square, :white) }
 
       before do
         en_passant_board.board.map! do |sqr|
@@ -409,7 +411,7 @@ describe Pawn do
       let(:blk_piece_sqr) { instance_double(Square, position: { x: 6, y: 5 }, piece: blk_piece) }
       let(:prev_blk_piece_sqr) { instance_double(Square, position: { x: 6, y: 7 }) }
       let(:start_square) { instance_double(Square, position: { x: 5, y: 5 }) }
-      subject(:wht_pawn) { described_class.new(:white, '♙', start_square) }
+      subject(:wht_pawn) { described_class.new(start_square, :white) }
 
       before do
         en_passant_board.board.map! do |sqr|
@@ -438,7 +440,7 @@ describe Pawn do
       let(:wht_pawn_dbl_sqr) { instance_double(Square, position: { X: 6, y: 4 }, piece: wht_pawn_dbl) }
       let(:prev_wht_pawn_dbl_sqr) { instance_double(Square, position: { x: 6, y: 2 }) }
       let(:start_square) { instance_double(Square, position: { x: 5, y: 4 }) }
-      subject(:wht_pawn) { described_class.new(:white, '♙', start_square) }
+      subject(:wht_pawn) { described_class.new(start_square, :white) }
 
       before do
         en_passant_board.board.map! do |sqr|
@@ -456,7 +458,7 @@ describe Pawn do
         allow(en_passant_board).to receive(:recorded_moves).and_return([[prev_wht_pawn_dbl_sqr, wht_pawn_dbl_sqr]])
       end
 
-      it 'returns empty array' do
+      it 'doesn\' work with pieces of the same color' do
         expect(wht_pawn.en_passant_move(en_passant_board, 1)).to be_empty
       end
     end
