b740243500a1a11221f3a9c8ce57e59f1ef1b82f
Magmusacy <xpfk.098@gmail.com>
2021-04-15 20:30:04 +0200

    Remove invalid test for #move method

--- spec/pieces/shared_piece_spec.rb
+++ spec/pieces/shared_piece_spec.rb
@@ -3,14 +3,6 @@ RSpec.shared_examples 'base class methods names' do
     it 'responds to #move' do
       expect(subject).to respond_to(:move)
     end
-
-    let(:board) { double('board') }
-    it '#move sends :get_square message to board' do
-      position = { x: 2, y: 3 }
-      allow(board).to receive(:get_square)
-      expect(board).to receive(:get_square)
-      subject.move(position, board)
-    end
   end
 end
 
