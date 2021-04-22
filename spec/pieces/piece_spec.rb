94f903f2c5c816f43969096a63d822f227bb04d9
Magmusacy <xpfk.098@gmail.com>
2021-04-22 17:02:29 +0200

    Add tests for #reject_related_squares method

--- spec/pieces/piece_spec.rb
+++ spec/pieces/piece_spec.rb
@@ -61,4 +61,24 @@ describe Piece do
       expect { piece.move(square, board) }.to change { piece.location }.to(square)
     end
   end
+
+  describe '#reject_related_squares' do
+    context 'when given an array of squares' do
+      subject(:piece) { described_class.new(nil, :white, nil) }
+
+      let(:piece_wht) { double('Piece', color: :white) }
+      let(:piece_blk) { double('Piece', color: :black) }
+      let(:square_wht1) { instance_double(Square, piece: piece_wht, taken?: true) }
+      let(:square_wht2) { instance_double(Square, piece: piece_wht, taken?: true) }
+      let(:square_blk1) { instance_double(Square, piece: piece_blk, taken?: true) }
+      let(:square_blk2) { instance_double(Square, piece: piece_blk, taken?: true) }
+
+      it 'returns modified array without squares that have the same @piece.color as the calling object' do
+        squares = [square_wht1, square_wht2, square_blk1, square_blk2]
+        result = piece.reject_related_squares(squares)
+        expected = [square_blk1, square_blk2]
+        expect(result).to match_array(expected)
+      end
+    end
+  end
 end
