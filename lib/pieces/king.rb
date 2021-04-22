4af1ae6e1d0a297d6ef9b02954387f3b486d3363
Magmusacy <xpfk.098@gmail.com>
2021-04-22 19:00:54 +0200

    Add King class

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,4 +1,25 @@
 require_relative 'piece'
 
 class King < Piece
+  def legal_moves(board)
+    moves = [horizontal_move(board, 1), horizontal_move(board, -1),
+             vertical_move(board, 1), vertical_move(board, -1),
+             diagonal_move(board, 1, 1), diagonal_move(board, 1, -1),
+             diagonal_move(board, -1, 1), diagonal_move(board, -1, -1)].flatten
+  end
+
+  def horizontal_move(board, x)
+    move_square = [find_relative_square(board, x: x)].compact
+    reject_related_squares(move_square)
+  end
+
+  def vertical_move(board, y)
+    move_square = [find_relative_square(board, y: y)].compact
+    reject_related_squares(move_square)
+  end
+
+  def diagonal_move(board, x, y)
+    move_square = [find_relative_square(board, x: x, y: y)]
+    reject_related_squares(move_square)
+  end
 end
