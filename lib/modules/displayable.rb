3288ab2faccf5e7342f31f131856b864c284f7fd
Magmusacy <xpfk.098@gmail.com>
2021-04-28 18:24:09 +0200

    Change #square_to_string method to return string instead of square.piece object

--- lib/modules/displayable.rb
+++ lib/modules/displayable.rb
@@ -16,15 +16,9 @@ module Displayable
   def display_row(array, row_colors, legal_moves = [])
     array.each_with_index do |sqr, idx|
       if legal_moves.include?(sqr)
-<<<<<<< HEAD
-        print colorize(sqr.piece, 41)
-      else
-        print colorize(sqr.piece, row_colors[idx])
-=======
         print colorize(square_to_string(sqr), 41)
       else
         print colorize(square_to_string(sqr), row_colors[idx])
->>>>>>> master
       end
     end
   end
@@ -32,12 +26,9 @@ module Displayable
   def colorize(string, color_code)
     "\e[#{color_code}m#{string}\e[0m"
   end
-<<<<<<< HEAD
-=======
 
   def square_to_string(square)
-    square.piece.is_a?(String) ? square.piece : " #{square.piece.icon} "
+    square.piece.nil? ? '   ' : " #{square.piece.icon} "
   end
->>>>>>> master
 end
 
