58ee9a2987bdc7f7d5b82c2872d27654f8f70eb2
Magmusacy <xpfk.098@gmail.com>
2021-04-28 16:40:39 +0200

    Add tests for SquareAccessor module

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative '../lib/modules/square_accessor'
 require_relative '../lib/board'
 require_relative '../lib/square'
 require_relative '../lib/pieces/piece'
@@ -32,37 +33,101 @@ describe Board do
     context 'when initialized board with black_pieces' do
       let(:white_piece) { double('Piece', position: { x: 2, y: 2 }) }
       let(:black_piece) { double('Piece', position: { x: 7, y: 2 }) }
-      let(:square_dbl) { instance_double(Square, position: { x: 7, y: 2 }) }
+      let(:blk_square) { instance_double(Square, position: { x: 7, y: 2 }) }
 
       before do
-        allow(square_dbl).to receive(:update_piece).with(black_piece)
+        allow(chess_board).to receive(:get_square).with({ x: 7, y: 2 }).and_return(blk_square)
+        allow(blk_square).to receive(:update_piece).with(black_piece)
         allow(black_piece).to receive(:update_position)
-        chess_board.board = [square_dbl]
+        chess_board.board = [blk_square]
       end
 
       it 'sends :update_piece message with Piece argument to Square with the same default position' do
-        expect(square_dbl).to receive(:update_piece).with(black_piece)
+        expect(blk_square).to receive(:update_piece).with(black_piece)
         chess_board.assign_pieces([black_piece])
       end
 
       it 'sends :update_position message with Square object for Piece that is being assigned to the Square' do
-        expect(black_piece).to receive(:update_position).with(square_dbl)
+        expect(black_piece).to receive(:update_position).with(blk_square)
         chess_board.assign_pieces([black_piece])
       end
     end
   end
 
   describe '#get_square' do
-    context 'when given { x: 2, y: 5 } as an argument' do
-      let(:square_dbl) { instance_double(Square, position: { x: 2, y: 5 }) }
-      let(:board) { [square_dbl] }
+    context 'when given position { x: 2, y: 2 }' do
+      let(:exp_sqr_22) { instance_double(Square, position: { x: 2, y:2 }) }
+      subject(:chess_board) { described_class.new([exp_sqr_22]) }
 
-      subject(:chess_board) { described_class.new(board) }
+      it 'returns only one square with given position' do
+        position = { x: 2, y: 2 }
+        result = chess_board.get_square(position)
+        expect(result).to eq(exp_sqr_22)
+      end
+    end
+  end
 
-      it 'returns square object with position: { x: 2, y: 5 }' do
-        position = { x: 2, y: 5 }
-        square_object = chess_board.get_square(position)
-        expect(square_object.position).to eq({ x: 2, y: 5 })
+  describe '#get_relative_square' do
+    context 'when given initial_square { x: 2, y: 2 } and x: 1, y: 1' do
+      let(:initial_square_22) { instance_double(Square, position: { x: 2, y: 2 }) }
+      let(:expected_square_33) { instance_double(Square, position: { x: 3, y: 3 }) }
+
+      it 'returns square { x: 3, y: 3 }' do
+        allow(chess_board).to receive(:get_square).with({ x: 3, y: 3 }).and_return(expected_square_33)
+        result = chess_board.get_relative_square(initial_square_22, x: 1, y: 1)
+        expect(result).to eq(expected_square_33)
+      end
+    end
+
+    context 'when given initial_square { x: 1, y: 4 } and x: 4, y: -2' do
+      let(:initial_square_14) { instance_double(Square, position: { x: 1, y: 4 }) }
+      let(:expected_square_52) { instance_double(Square, position: { x: 5, y: 2 }) }
+
+      it 'returns square { x: 5, y: 2 }' do
+        allow(chess_board).to receive(:get_square).with({ x: 5, y: 2 }).and_return(expected_square_52)
+        result = chess_board.get_relative_square(initial_square_14, x: 4, y: -2)
+        expect(result).to eq(expected_square_52)
+      end
+    end
+  end
+
+  describe '#squares_taken_by' do
+    let(:wht_piece) { instance_double(Piece, color: :white) }
+    let(:blk_piece) { instance_double(Piece, color: :black) }
+    let(:wht_squares) do
+      [
+        instance_double(Square, piece: wht_piece),
+        instance_double(Square, piece: wht_piece)
+      ]
+    end
+    let(:blk_squares) do
+      [
+        instance_double(Square, piece: blk_piece),
+        instance_double(Square, piece: blk_piece)
+      ]
+    end
+    let(:empty_squares) do
+      [
+        instance_double(Square, piece: '   '),
+        instance_double(Square, piece: '   ')
+      ]
+    end
+    let(:board) { [wht_squares, blk_squares, empty_squares].flatten }
+    subject(:chess_board) { described_class.new(board) }
+
+    context 'when given :black as an argument' do
+      it 'returns all squares with :black pieces on them' do
+        color = :black
+        result = chess_board.squares_taken_by(color)
+        expect(result).to match_array(blk_squares)
+      end
+    end
+
+    context 'when given :white as an argument' do
+      it 'returns all squares with :white pieces on them' do
+        color = :white
+        result = chess_board.squares_taken_by(color)
+        expect(result).to match_array(wht_squares)
       end
     end
   end
