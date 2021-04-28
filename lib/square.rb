da41faeaff6e1942fb03c11db8af8e5b6d6d633d
Magmusacy <xpfk.098@gmail.com>
2021-04-28 18:25:26 +0200

    Change default/free square @piece attribute to be nil instead of string

--- lib/square.rb
+++ lib/square.rb
@@ -4,16 +4,16 @@
 class Square
   attr_reader :position, :piece
 
-  def initialize(position, piece = '   ')
+  def initialize(position, piece = nil)
     @position = { x: position.first, y: position.last }
     @piece = piece
   end
 
-  def update_piece(new_piece = '   ')
+  def update_piece(new_piece = nil)
     @piece = new_piece
   end
 
   def taken?
-    !piece.is_a?(String)
+    !piece.nil?
   end
 end
