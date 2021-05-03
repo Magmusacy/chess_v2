e40a5429e621b944a71fb8038f2730d61f6b9e00
Magmusacy <xpfk.098@gmail.com>
2021-05-03 21:13:12 +0200

    Add tests for #clone_move and #discard_illegal_moves from DiscardIllegalMoves module

--- spec/pieces/piece_spec.rb
+++ spec/pieces/piece_spec.rb
@@ -50,6 +50,79 @@ describe Piece do
     end
   end
 
+  describe '#clone_move' do
+    let(:clone_board) { instance_double(Board) }
+    let(:real_board) { instance_double(Board) }
+    let(:clone_square) { instance_double(Square, piece: nil) }
+    let(:real_square) { instance_double(Square, piece: nil) }
+    let(:clone_piece) { instance_double(Piece) }
+    subject(:real_piece) { described_class.new(nil, :white, nil) }
+
+    before do
+      allow(real_piece).to receive(:clone).and_return(clone_piece)
+      allow(real_board).to receive(:clone).and_return(clone_board)
+      allow(real_square).to receive(:clone).and_return(clone_square)
+      allow(clone_piece).to receive(:move).with(clone_square, clone_board)
+    end
+
+    it 'sends :move message to cloned piece, with cloned square and cloned board' do
+      expect(clone_piece).to receive(:move).with(clone_square, clone_board)
+      real_piece.clone_move(real_board, real_piece, real_square)
+    end
+
+    it 'returns cloned board' do
+      result = real_piece.clone_move(real_board, real_piece, real_square)
+      expect(result).to eq(clone_board)
+    end
+  end
+
+  describe '#discard_illegal_moves' do
+    let(:opponent_color) { :black }
+    let(:real_color) { :white }
+    let(:real_board) { instance_double(Board) }
+    subject(:real_piece) { described_class.new(nil, real_color, nil) }
+
+    context 'when possible_moves has 1 move' do
+    let(:real_square1) { instance_double(Square, piece: nil) }
+    let(:possible_moves) { [real_square1] }
+    let(:clone_board1) { instance_double(Board) }
+
+      context 'when after that 1 move there is check condition' do
+        before do
+          allow(real_piece).to receive(:clone_move).with(real_board, real_piece, real_square1).and_return(clone_board1)
+          allow(real_piece).to receive(:check?).with(clone_board1, real_color, opponent_color).and_return(true)
+        end
+
+        it 'returns empty array' do
+          result = real_piece.discard_illegal_moves(real_board, opponent_color, possible_moves)
+          expect(result).to be_empty
+        end
+      end
+    end
+
+    context 'when possible_moves has 2 moves' do
+      let(:real_square1) { instance_double(Square, piece: nil) }
+      let(:real_square2) { instance_double(Square, piece: nil) }
+      let(:clone_board1) { instance_double(Board) }
+      let(:clone_board2) { instance_double(Board) }
+      let(:possible_moves) { [real_square1, real_square2] }
+
+      context 'when after first move there is a check condition, but after the second move there is not' do
+        before do
+          allow(real_piece).to receive(:clone_move).with(real_board, real_piece, real_square1).and_return(clone_board1)
+          allow(real_piece).to receive(:clone_move).with(real_board, real_piece, real_square2).and_return(clone_board2)
+          allow(real_piece).to receive(:check?).with(clone_board1, real_color, opponent_color).and_return(true)
+          allow(real_piece).to receive(:check?).with(clone_board2, real_color, opponent_color).and_return(false)
+        end
+
+        it 'returns array with only second move' do
+          result = real_piece.discard_illegal_moves(real_board, opponent_color, possible_moves)
+          expect(result).to eq([real_square2])
+        end
+      end
+    end
+  end
+
   describe '#reject_related_squares' do
     context 'when given an array of squares' do
       subject(:piece) { described_class.new(nil, :white, nil) }
