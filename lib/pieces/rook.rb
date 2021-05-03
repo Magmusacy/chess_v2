25b0b46ee18f32800e2dd16a632adadba9e98e6e
Magmusacy <xpfk.098@gmail.com>
2021-05-03 19:37:17 +0200

    Add #legal_moves method and include SharedMovement::RookMovement module

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -1,4 +1,11 @@
 require_relative 'piece'
+require_relative '../modules/shared_movement'
 
 class Rook < Piece
+  include SharedMovement::RookMovement
+
+  def legal_moves(board)
+    [horizontal_move(board, 1), horizontal_move(board, -1),
+     vertical_move(board, 1), vertical_move(board, -1)].flatten
+  end
 end
