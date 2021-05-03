bca12377a0ce1fdf79ce683432db329318a637c9
Magmusacy <xpfk.098@gmail.com>
2021-05-03 19:45:48 +0200

    Add #legal_moves method and include SharedMovement::BishopMovement module

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -1,4 +1,11 @@
 require_relative 'piece'
+require_relative '../modules/shared_movement'
 
 class Bishop < Piece
+  include SharedMovement::BishopMovement
+
+  def legal_moves(board)
+    [diagonal_move(board, 1, 1), diagonal_move(board, 1, -1),
+     diagonal_move(board, -1, 1), diagonal_move(board, -1, -1)].flatten
+  end
 end
