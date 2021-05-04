df73a6556809a8884725c0d200e1dc748149a1a9
Magmusacy <xpfk.098@gmail.com>
2021-05-04 16:14:05 +0200

    Add #discard_illegal_moves method call to #legal_moves method

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -5,7 +5,8 @@ class Bishop < Piece
   include SharedMovement::BishopMovement
 
   def legal_moves(board)
-    [diagonal_move(board, 1, 1), diagonal_move(board, 1, -1),
-     diagonal_move(board, -1, 1), diagonal_move(board, -1, -1)].flatten
+    moves = [diagonal_move(board, 1, 1), diagonal_move(board, 1, -1),
+             diagonal_move(board, -1, 1), diagonal_move(board, -1, -1)].flatten
+    discard_illegal_moves(board, opponent_color, moves)
   end
 end
--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -5,7 +5,8 @@ class Rook < Piece
   include SharedMovement::RookMovement
 
   def legal_moves(board)
-    [horizontal_move(board, 1), horizontal_move(board, -1),
-     vertical_move(board, 1), vertical_move(board, -1)].flatten
+    moves = [horizontal_move(board, 1), horizontal_move(board, -1),
+             vertical_move(board, 1), vertical_move(board, -1)].flatten
+    discard_illegal_moves(board, opponent_color, moves)
   end
 end
