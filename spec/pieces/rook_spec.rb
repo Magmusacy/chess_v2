b57ce98c0d46773d86c2cde49f04a11b71a8e80b
Magmusacy <xpfk.098@gmail.com>
2021-05-04 15:40:27 +0200

    Refactor some test and remove redundant tests

--- spec/pieces/rook_spec.rb
+++ spec/pieces/rook_spec.rb
@@ -16,45 +16,70 @@ describe Rook do
 
   describe '#legal_moves' do
     subject(:rook_moves) { described_class.new }
-    let(:move_square) { double('square') }
+    let(:legal) { [double('square')] }
 
-    context 'when invoked #horizontal_move 2 times, with x = 1 and x = -1' do
-      context 'when #horizontal_move returns 1 move square' do
+    before do
+      allow(rook_moves).to receive(:vertical_move).and_return([])
+      allow(rook_moves).to receive(:horizontal_move).and_return([])
+    end
 
-        before do
-          allow(rook_moves).to receive(:vertical_move).and_return([])
-          allow(rook_moves).to receive(:horizontal_move).with(chess_board, 1).and_return(move_square)
-          allow(rook_moves).to receive(:horizontal_move).with(chess_board, -1).and_return(move_square)
-        end
+    context 'when only #horizontal_move with x = 1 returns a possible and legal move' do
+      before do
+        x = 1
+        allow(rook_moves).to receive(:horizontal_move).with(chess_board, x).and_return(legal)
+      end
 
-        it 'returns an array with 2 move squares' do
-          result = rook_moves.legal_moves(chess_board)
-          expect(result).to match_array([move_square, move_square])
-        end
+      it 'returns an array with 1 legal move' do
+        result = rook_moves.legal_moves(chess_board)
+        expect(result).to match_array(legal)
       end
     end
 
-    context 'when invoked #vertical_move 2 times, with y = 1 and y = -1' do
-      context 'when #vertical_move returns 1 move square' do
+    context 'when only #horizontal_move with x = -1 returns a possible and legal move' do
+      before do
+        x = -1
+        allow(rook_moves).to receive(:horizontal_move).with(chess_board, x).and_return(legal)
+      end
 
-        before do
-          allow(rook_moves).to receive(:horizontal_move).and_return([])
-          allow(rook_moves).to receive(:vertical_move).with(chess_board, 1).and_return(move_square)
-          allow(rook_moves).to receive(:vertical_move).with(chess_board, -1).and_return(move_square)
-        end
+      it 'returns an array with 1 legal move' do
+        result = rook_moves.legal_moves(chess_board)
+        expect(result).to match_array(legal)
+      end
+    end
 
-        it 'returns an array with 2 move squares' do
-          result = rook_moves.legal_moves(chess_board)
-          expect(result).to match_array([move_square, move_square])
-        end
+    context 'when only #vertical_move with y = 1 returns a possible and legal move' do
+      before do
+        y = 1
+        allow(rook_moves).to receive(:vertical_move).with(chess_board, y).and_return(legal)
+      end
+
+      it 'returns an array with 1 legal move' do
+        result = rook_moves.legal_moves(chess_board)
+        expect(result).to match_array(legal)
+      end
+    end
+
+    context 'when only #vertical_move with y = -1 returns a possible and legal move' do
+      before do
+        y = -1
+        allow(rook_moves).to receive(:vertical_move).with(chess_board, y).and_return(legal)
+      end
+
+      it 'returns an array with 1 legal move' do
+        result = rook_moves.legal_moves(chess_board)
+        expect(result).to match_array(legal)
       end
     end
   end
 
   describe '#horizontal_move' do
+    before do
+      allow(chess_board).to receive(:get_relative_square)
+    end
+
     context 'when :white Rook is on square { x: 4, y: 4 }' do
       let(:start_square_44) { double('square', position: { x: 4, y: 4 }) }
-      subject(:rook_44) { described_class.new(start_square_44, :white) }
+      subject(:wht_rook_44) { described_class.new(start_square_44, :white) }
 
       context 'when given x = 1' do
         let(:x) { 1 }
@@ -70,56 +95,15 @@ describe Rook do
           end
 
           before do
-            allow(chess_board).to receive(:get_relative_square)
             allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: 1).and_return(exp_sqr[0])
             allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: 2).and_return(exp_sqr[1])
             allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: 3).and_return(exp_sqr[2])
             allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: 4).and_return(exp_sqr[3])
           end
 
-          it 'returns 4 squares with x: { 5, 6, 7, 8 } respectively and y: 4' do
-            result = rook_44.horizontal_move(chess_board, x)
+          it 'returns 4 squares { x: 5, y: 4 }, { x: 6, y: 4 }, { x: 7, y: 4 }, { x: 8, y: 4 }' do
+            result = wht_rook_44.horizontal_move(chess_board, x)
             expect(result).to match_array(exp_sqr)
-          end
-        end
-
-        context 'when only square { x: 6, y: 4 } is taken by :black Piece' do
-          let(:blk_piece) { double('piece', color: :black) }
-          let(:exp_sqr) do
-            [
-              double('Square', position: { x: 5, y: 4 }, taken?: false, piece: nil),
-              double('Square', position: { x: 6, y: 4 }, taken?: true, piece: blk_piece)
-            ]
-          end
-
-          before do
-            allow(chess_board).to receive(:get_relative_square)
-            allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: 1).and_return(exp_sqr[0])
-            allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: 2).and_return(exp_sqr[1])
-          end
-
-          it 'returns 2 squares with x: { 5, 6 } respectively and y: 4' do
-            result = rook_44.horizontal_move(chess_board, x)
-            expect(result).to match_array(exp_sqr)
-          end
-        end
-
-        context 'when square { x: 5, y: 4 } is taken by Piece with :white color' do
-          let(:wht_piece) { double('piece', color: :white) }
-          let(:exp_sqr) do
-            [
-              double('Square', position: { x: 5, y: 4 }, taken?: true, piece: wht_piece),
-            ]
-          end
-
-          before do
-            allow(chess_board).to receive(:get_relative_square)
-            allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: 1).and_return(exp_sqr[0])
-          end
-
-          it 'returns empty array' do
-            result = rook_44.horizontal_move(chess_board, x)
-            expect(result).to be_empty
           end
         end
       end
@@ -138,54 +122,82 @@ describe Rook do
           end
 
           before do
-            allow(chess_board).to receive(:get_relative_square)
             allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: -1).and_return(exp_sqr[0])
             allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: -2).and_return(exp_sqr[1])
             allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: -3).and_return(exp_sqr[2])
           end
 
-          it 'returns 4 squares with x: { 3, 2, 1 } respectively and y: 4' do
-            result = rook_44.horizontal_move(chess_board, x)
+          it 'returns 4 squares { x: 3, y: 4 }, { x: 2, y: 4 }, { x: 1, y: 4 }' do
+            result = wht_rook_44.horizontal_move(chess_board, x)
             expect(result).to match_array(exp_sqr)
           end
         end
+      end
 
-        context 'when only square { x: 2, y: 4 } is taken by :black Piece' do
-          let(:blk_piece) { double('piece', color: :black) }
+      context 'when there is :black Piece on square that Rook can pass through' do
+        let(:blk_piece) { double('piece', color: :black) }
+        let(:exp_sqr) do
+          [
+            double('Square', position: { x: 5, y: 4 }, taken?: false, piece: nil),
+            double('Square', position: { x: 6, y: 4 }, taken?: false, piece: nil),
+            double('Square', position: { x: 7, y: 4 }, taken?: true, piece: blk_piece)
+          ]
+        end
+
+        before do
+          allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: 1).and_return(exp_sqr[0])
+          allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: 2).and_return(exp_sqr[1])
+          allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: 3).and_return(exp_sqr[2])
+        end
+
+        it 'returns an array of squares passed by Rook until it encountered :black Piece, including it' do
+          x = 1
+          expected = exp_sqr
+          result = wht_rook_44.horizontal_move(chess_board, x)
+          expect(result).to match_array(expected)
+        end
+      end
+
+      context 'when there is another :white Piece on square that Rook can pass through' do
+        let(:wht_piece) { double('piece', color: :white) }
+
+        context 'when Rook has already passed a few squares' do
           let(:exp_sqr) do
             [
-              double('Square', position: { x: 3, y: 4 }, taken?: false, piece: nil),
-              double('Square', position: { x: 2, y: 4 }, taken?: true, piece: blk_piece)
+              double('Square', position: { x: 5, y: 4 }, taken?: false, piece: nil),
+              double('Square', position: { x: 6, y: 4 }, taken?: false, piece: nil),
+              double('Square', position: { x: 7, y: 4 }, taken?: true, piece: wht_piece)
             ]
           end
 
           before do
-            allow(chess_board).to receive(:get_relative_square)
-            allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: -1).and_return(exp_sqr[0])
-            allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: -2).and_return(exp_sqr[1])
+            allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: 1).and_return(exp_sqr[0])
+            allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: 2).and_return(exp_sqr[1])
+            allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: 3).and_return(exp_sqr[2])
           end
 
-          it 'returns 2 squares with x: { 3, 2 } respectively and y: 4' do
-            result = rook_44.horizontal_move(chess_board, x)
-            expect(result).to match_array(exp_sqr)
+          it 'returns an array of squares passed by Rook until it encountered another :white Piece, excluding it' do
+            x = 1
+            expected = exp_sqr.first(2)
+            result = wht_rook_44.horizontal_move(chess_board, x)
+            expect(result).to match_array(expected)
           end
         end
 
-        context 'when square { x: 3, y: 4 } is taken by Piece with :white color' do
-          let(:wht_piece) { double('piece', color: :white) }
+        context 'when Rook hasn\'t made any move yet before encountering that square' do
           let(:exp_sqr) do
             [
-              double('Square', position: { x: 3, y: 4 }, taken?: true, piece: wht_piece),
+              double('Square', position: { x: 5, y: 4 }, taken?: true, piece: wht_piece),
             ]
           end
 
           before do
-            allow(chess_board).to receive(:get_relative_square)
-            allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: -1).and_return(exp_sqr[0])
+            allow(chess_board).to receive(:get_relative_square).with(start_square_44, x: 1).and_return(exp_sqr[0])
           end
 
-          it 'returns empty array' do
-            result = rook_44.horizontal_move(chess_board, x)
+          it 'returns an empty array' do
+            x = 1
+            result = wht_rook_44.horizontal_move(chess_board, x)
             expect(result).to be_empty
           end
         end
@@ -194,9 +206,13 @@ describe Rook do
   end
 
   describe '#vertical_move' do
+    before do
+      allow(chess_board).to receive(:get_relative_square)
+    end
+
     context 'when :white Rook is on square { x: 4, y: 4 }' do
       let(:start_square_44) { double('square', position: { x: 4, y: 4 }) }
-      subject(:rook_44) { described_class.new(start_square_44, :white) }
+      subject(:wht_rook_44) { described_class.new(start_square_44, :white) }
 
       context 'when given y = 1' do
         let(:y) { 1 }
@@ -212,7 +228,6 @@ describe Rook do
           end
 
           before do
-            allow(chess_board).to receive(:get_relative_square)
             allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: 1).and_return(exp_sqr[0])
             allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: 2).and_return(exp_sqr[1])
             allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: 3).and_return(exp_sqr[2])
@@ -220,48 +235,8 @@ describe Rook do
           end
 
           it 'returns 4 squares with x: 4 and y: { 5, 6, 7, 8 } respectively' do
-            result = rook_44.vertical_move(chess_board, y)
+            result = wht_rook_44.vertical_move(chess_board, y)
             expect(result).to match_array(exp_sqr)
-          end
-        end
-
-        context 'when only square { x: 4, y: 6 } is taken by :black Piece' do
-          let(:blk_piece) { double('piece', color: :black) }
-          let(:exp_sqr) do
-            [
-              double('Square', position: { x: 4, y: 5 }, taken?: false, piece: nil),
-              double('Square', position: { x: 4, y: 6 }, taken?: true, piece: blk_piece)
-            ]
-          end
-
-          before do
-            allow(chess_board).to receive(:get_relative_square)
-            allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: 1).and_return(exp_sqr[0])
-            allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: 2).and_return(exp_sqr[1])
-          end
-
-          it 'returns 2 squares with x: 4 and y: { 5, 6 } respectively' do
-            result = rook_44.vertical_move(chess_board, y)
-            expect(result).to match_array(exp_sqr)
-          end
-        end
-
-        context 'when square { x: 4, y: 5 } is taken by Piece with :white color' do
-          let(:wht_piece) { double('piece', color: :white) }
-          let(:exp_sqr) do
-            [
-              double('Square', position: { x: 4, y: 5 }, taken?: true, piece: wht_piece),
-            ]
-          end
-
-          before do
-            allow(chess_board).to receive(:get_relative_square)
-            allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: 1).and_return(exp_sqr[0])
-          end
-
-          it 'returns empty array' do
-            result = rook_44.vertical_move(chess_board, y)
-            expect(result).to be_empty
           end
         end
       end
@@ -279,54 +254,82 @@ describe Rook do
           end
 
           before do
-            allow(chess_board).to receive(:get_relative_square)
             allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: -1).and_return(exp_sqr[0])
             allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: -2).and_return(exp_sqr[1])
             allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: -3).and_return(exp_sqr[2])
           end
 
           it 'returns 3 squares with x: 4 and y: { 3, 2, 1 } respectively' do
-            result = rook_44.vertical_move(chess_board, y)
+            result = wht_rook_44.vertical_move(chess_board, y)
             expect(result).to match_array(exp_sqr)
           end
         end
+      end
 
-        context 'when only square { x: 4, y: 2 } is taken by :black Piece' do
-          let(:blk_piece) { double('piece', color: :black) }
+      context 'when there is :black Piece on square that Rook can pass through' do
+        let(:blk_piece) { double('piece', color: :black) }
+        let(:exp_sqr) do
+          [
+            double('Square', position: { x: 4, y: 5 }, taken?: false, piece: nil),
+            double('Square', position: { x: 4, y: 6 }, taken?: false, piece: nil),
+            double('Square', position: { x: 4, y: 7 }, taken?: true, piece: blk_piece)
+          ]
+        end
+
+        before do
+          allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: 1).and_return(exp_sqr[0])
+          allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: 2).and_return(exp_sqr[1])
+          allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: 3).and_return(exp_sqr[2])
+        end
+
+        it 'returns an array of squares passed by Rook until it encountered :black Piece, including it' do
+          y = 1
+          expected = exp_sqr
+          result = wht_rook_44.vertical_move(chess_board, y)
+          expect(result).to match_array(expected)
+        end
+      end
+
+      context 'when there is another :white Piece on square that Rook can pass through' do
+        let(:wht_piece) { double('piece', color: :white) }
+
+        context 'when Rook has already passed a few squares' do
           let(:exp_sqr) do
             [
-              double('Square', position: { x: 4, y: 3 }, taken?: false, piece: nil),
-              double('Square', position: { x: 4, y: 2 }, taken?: true, piece: blk_piece)
+              double('Square', position: { x: 4, y: 5 }, taken?: false, piece: nil),
+              double('Square', position: { x: 4, y: 6 }, taken?: false, piece: nil),
+              double('Square', position: { x: 4, y: 7 }, taken?: true, piece: wht_piece)
             ]
           end
 
           before do
-            allow(chess_board).to receive(:get_relative_square)
-            allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: -1).and_return(exp_sqr[0])
-            allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: -2).and_return(exp_sqr[1])
+            allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: 1).and_return(exp_sqr[0])
+            allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: 2).and_return(exp_sqr[1])
+            allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: 3).and_return(exp_sqr[2])
           end
 
-          it 'returns 2 squares with x: 4 and y: { 3, 2 } respectively' do
-            result = rook_44.vertical_move(chess_board, y)
-            expect(result).to match_array(exp_sqr)
+          it 'returns an array of squares passed by Rook until it encountered another :white Piece, excluding it' do
+            y = 1
+            expected = exp_sqr.first(2)
+            result = wht_rook_44.vertical_move(chess_board, y)
+            expect(result).to match_array(expected)
           end
         end
 
-        context 'when square { x: 4, y: 3 } is taken by Piece with :white color' do
-          let(:wht_piece) { double('piece', color: :white) }
+        context 'when Rook hasn\'t made any move yet before encountering that square' do
           let(:exp_sqr) do
             [
-              double('Square', position: { x: 4, y: 3 }, taken?: true, piece: wht_piece),
+              double('Square', position: { x: 4, y: 5 }, taken?: true, piece: wht_piece),
             ]
           end
 
           before do
-            allow(chess_board).to receive(:get_relative_square)
-            allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: -1).and_return(exp_sqr[0])
+            allow(chess_board).to receive(:get_relative_square).with(start_square_44, y: 1).and_return(exp_sqr[0])
           end
 
-          it 'returns empty array' do
-            result = rook_44.vertical_move(chess_board, y)
+          it 'returns an empty array' do
+            y = 1
+            result = wht_rook_44.vertical_move(chess_board, y)
             expect(result).to be_empty
           end
         end
