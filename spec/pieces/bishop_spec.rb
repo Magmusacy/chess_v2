aabb021baa79deba5567115732279275f2b2ccb0
Magmusacy <xpfk.098@gmail.com>
2021-05-04 15:00:59 +0200

    Refactored some of the test, removed redundant tests

--- spec/pieces/bishop_spec.rb
+++ spec/pieces/bishop_spec.rb
@@ -16,28 +16,32 @@ describe Bishop do
 
   describe '#legal_moves' do
     subject(:bishop_moves) { described_class.new(nil, :white) }
-    let(:legal_moves) { [double('square'), double('square')] }
-    let(:opponent_color) { :black }
+    let(:moves) { [double('square'), double('square')] }
+    let(:enemy_color) { :black }
+    let(:illegal) { [moves.first] }
+    let(:legal) { [moves.last] }
 
-    context 'when all move methods returned 2 possible moves and only 1 of them is legal' do
+    before do
+      allow(bishop_moves).to receive(:diagonal_move).and_return([])
+    end
+
+    context 'when all move methods returned 2 moves and only 1 of them is legal' do
       before do
-        allow(bishop_moves).to receive(:diagonal_move).and_return([])
-        allow(bishop_moves).to receive(:diagonal_move).with(chess_board, 1, 1).and_return([legal_moves[0]])
-        allow(bishop_moves).to receive(:diagonal_move).with(chess_board, -1, -1).and_return([legal_moves[1]])
-        allow(bishop_moves).to receive(:discard_illegal_moves).with(chess_board, opponent_color, legal_moves).and_return([legal_moves[1]])
+        allow(bishop_moves).to receive(:diagonal_move).with(chess_board, 1, 1).and_return(illegal)
+        allow(bishop_moves).to receive(:diagonal_move).with(chess_board, -1, -1).and_return(legal)
+        allow(bishop_moves).to receive(:discard_illegal_moves).with(chess_board, enemy_color, moves).and_return(legal)
       end
 
       it 'returns only that 1 legal move' do
         result = bishop_moves.legal_moves(chess_board)
-        expect(result).to match_array([legal_moves[1]])
+        expect(result).to match_array(legal)
       end
     end
 
-    context 'when all move methods returned 1 possible moves and that move is not legal' do
+    context 'when all move methods returned 1 move and that move is not legal' do
       before do
-        allow(bishop_moves).to receive(:diagonal_move).and_return([])
-        allow(bishop_moves).to receive(:diagonal_move).with(chess_board, 1, 1).and_return([legal_moves[1]])
-        allow(bishop_moves).to receive(:discard_illegal_moves).with(chess_board, opponent_color, [legal_moves[1]]).and_return([])
+        allow(bishop_moves).to receive(:diagonal_move).with(chess_board, 1, 1).and_return(illegal)
+        allow(bishop_moves).to receive(:discard_illegal_moves).with(chess_board, enemy_color, illegal).and_return([])
       end
 
       it 'returns empty array' do
@@ -47,42 +51,68 @@ describe Bishop do
     end
 
 
-    context 'when invoked #diagonal_move 2 times, with x = 1, y = 1 and x = -1, y = -1' do
-      context 'when #diagonal_move returns 1 move square' do
+    context 'when only #diagonal_move with x = 1, y = 1 returns a possible and legal move' do
+      before do
+        x = 1
+        y = 1
+        allow(bishop_moves).to receive(:diagonal_move).with(chess_board, x, y).and_return(legal)
+        allow(bishop_moves).to receive(:discard_illegal_moves).and_return(legal)
+      end
 
-        before do
-          allow(bishop_moves).to receive(:diagonal_move).and_return([])
-          allow(bishop_moves).to receive(:diagonal_move).with(chess_board, 1, 1).and_return(legal_moves[0])
-          allow(bishop_moves).to receive(:diagonal_move).with(chess_board, -1, -1).and_return(legal_moves[1])
-          allow(bishop_moves).to receive(:discard_illegal_moves).and_return(legal_moves)
-        end
-
-        it 'returns an array with 2 move squares' do
-          result = bishop_moves.legal_moves(chess_board)
-          expect(result).to match_array(legal_moves)
-        end
+      it 'returns an array with 1 legal move' do
+        result = bishop_moves.legal_moves(chess_board)
+        expect(result).to match_array(legal)
       end
     end
 
-    context 'when invoked #diagonal_move 2 times, with x = 1, y = -1 and x = -1, y = 1' do
-      context 'when #vertical_move returns 1 move square' do
+    context 'when only #diagonal_move with x = 1, y = -1 returns a possible and legal move' do
+      before do
+        x = 1
+        y = -1
+        allow(bishop_moves).to receive(:diagonal_move).with(chess_board, x, y).and_return(legal)
+        allow(bishop_moves).to receive(:discard_illegal_moves).and_return(legal)
+      end
 
-        before do
-          allow(bishop_moves).to receive(:diagonal_move).and_return([])
-          allow(bishop_moves).to receive(:diagonal_move).with(chess_board, 1, -1).and_return(legal_moves[0])
-          allow(bishop_moves).to receive(:diagonal_move).with(chess_board, -1, 1).and_return(legal_moves[1])
-          allow(bishop_moves).to receive(:discard_illegal_moves).and_return(legal_moves)
-        end
+      it 'returns an array with 1 legal move' do
+        result = bishop_moves.legal_moves(chess_board)
+        expect(result).to match_array(legal)
+      end
+    end
 
-        it 'returns an array with 2 move squares' do
-          result = bishop_moves.legal_moves(chess_board)
-          expect(result).to match_array(legal_moves)
-        end
+    context 'when only #diagonal_move with x = -1, y = 1 returns a possible and legal move' do
+      before do
+        x = -1
+        y = 1
+        allow(bishop_moves).to receive(:diagonal_move).with(chess_board, x, y).and_return(legal)
+        allow(bishop_moves).to receive(:discard_illegal_moves).and_return(legal)
+      end
+
+      it 'returns an array with 1 legal move' do
+        result = bishop_moves.legal_moves(chess_board)
+        expect(result).to match_array(legal)
+      end
+    end
+
+    context 'when only #diagonal_move with x = -1, y = -1 returns a possible and legal move' do
+      before do
+        x = -1
+        y = -1
+        allow(bishop_moves).to receive(:diagonal_move).with(chess_board, x, y).and_return(legal)
+        allow(bishop_moves).to receive(:discard_illegal_moves).and_return(legal)
+      end
+
+      it 'returns an array with 1 legal move' do
+        result = bishop_moves.legal_moves(chess_board)
+        expect(result).to match_array(legal)
       end
     end
   end
 
-  context '#diagonal_move' do
+  describe '#diagonal_move' do
+    before do
+      allow(chess_board).to receive(:get_relative_square)
+    end
+
     context 'when given :white Bishop on square { x: 4, y: 4 }' do
       let(:start_sqr_44) { double('Square', position: { x: 4, y: 4 }) }
       subject(:wht_bishop_44) { described_class.new(start_sqr_44, :white) }
@@ -102,7 +132,6 @@ describe Bishop do
           end
 
           before do
-            allow(chess_board).to receive(:get_relative_square)
             allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 1, y: 1).and_return(exp_sqr[0])
             allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 2, y: 2).and_return(exp_sqr[1])
             allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 3, y: 3).and_return(exp_sqr[2])
@@ -112,48 +141,6 @@ describe Bishop do
           it 'returns 4 squares { x: 5, y: 5 }, { x: 6, y: 6 }, { x: 7, y: 7 }, { x: 8, y: 8 }' do
             result = wht_bishop_44.diagonal_move(chess_board, x, y)
             expect(result).to match_array(exp_sqr)
-          end
-        end
-
-        context 'when square { x: 7, y: 7 } is taken by :black Piece' do
-          let(:blk_piece) { double('piece', color: :black) }
-          let(:exp_sqr) do
-            [
-              double('Square', position: { x: 5, y: 5 }, taken?: false, piece: nil),
-              double('Square', position: { x: 6, y: 6 }, taken?: false, piece: nil),
-              double('Square', position: { x: 7, y: 7 }, taken?: true, piece: blk_piece)
-            ]
-          end
-
-          before do
-            allow(chess_board).to receive(:get_relative_square)
-            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 1, y: 1).and_return(exp_sqr[0])
-            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 2, y: 2).and_return(exp_sqr[1])
-            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 3, y: 3).and_return(exp_sqr[2])
-          end
-
-          it 'returns 3 squares { x: 5, y: 5 }, { x: 6, y: 6 }, { x: 7, y: 7 }' do
-            result = wht_bishop_44.diagonal_move(chess_board, x, y)
-            expect(result).to match_array(exp_sqr)
-          end
-        end
-
-        context 'when square { x: 5, y: 5 } is taken by :white Piece' do
-          let(:wht_piece) { double('piece', color: :white) }
-          let(:exp_sqr) do
-            [
-              double('Square', position: { x: 5, y: 5 }, taken?: true, piece: wht_piece),
-            ]
-          end
-
-          before do
-            allow(chess_board).to receive(:get_relative_square)
-            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 1, y: 1).and_return(exp_sqr[0])
-          end
-
-          it 'returns empty array' do
-            result = wht_bishop_44.diagonal_move(chess_board, x, y)
-            expect(result).to be_empty
           end
         end
       end
@@ -172,7 +159,6 @@ describe Bishop do
           end
 
           before do
-            allow(chess_board).to receive(:get_relative_square)
             allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: -1, y: 1).and_return(exp_sqr[0])
             allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: -2, y: 2).and_return(exp_sqr[1])
             allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: -3, y: 3).and_return(exp_sqr[2])
@@ -181,46 +167,6 @@ describe Bishop do
           it 'returns 3 squares { x: 3, y: 5 }, { x: 2, y: 6 }, { x: 1, y: 7 }' do
             result = wht_bishop_44.diagonal_move(chess_board, x, y)
             expect(result).to match_array(exp_sqr)
-          end
-        end
-
-        context 'when square { x: 2, y: 6 } is taken by :black Piece' do
-          let(:blk_piece) { double('piece', color: :black) }
-          let(:exp_sqr) do
-            [
-              double('Square', position: { x: 3, y: 5 }, taken?: false, piece: nil),
-              double('Square', position: { x: 2, y: 6 }, taken?: true, piece: blk_piece),
-            ]
-          end
-
-          before do
-            allow(chess_board).to receive(:get_relative_square)
-            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: -1, y: 1).and_return(exp_sqr[0])
-            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: -2, y: 2).and_return(exp_sqr[1])
-          end
-
-          it 'returns 2 squares { x: 3, y: 5 }, { x: 2, y: 6 }' do
-            result = wht_bishop_44.diagonal_move(chess_board, x, y)
-            expect(result).to match_array(exp_sqr)
-          end
-        end
-
-        context 'when square { x: 2, y: 6 } is taken by :white Piece' do
-          let(:wht_piece) { double('piece', color: :white) }
-          let(:exp_sqr) do
-            [
-              double('Square', position: { x: 3, y: 5 }, taken?: true, piece: wht_piece),
-            ]
-          end
-
-          before do
-            allow(chess_board).to receive(:get_relative_square)
-            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: -1, y: 1).and_return(exp_sqr[0])
-          end
-
-          it 'returns empty array' do
-            result = wht_bishop_44.diagonal_move(chess_board, x, y)
-            expect(result).to be_empty
           end
         end
       end
@@ -238,7 +184,6 @@ describe Bishop do
           end
 
           before do
-            allow(chess_board).to receive(:get_relative_square)
             allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 1, y: -1).and_return(exp_sqr[0])
             allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 2, y: -2).and_return(exp_sqr[1])
             allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 3, y: -3).and_return(exp_sqr[2])
@@ -249,47 +194,6 @@ describe Bishop do
             expect(result).to match_array(exp_sqr)
           end
         end
-
-        context 'when square { x: 6, y: 2 } is taken by :black Piece' do
-          let(:blk_piece) { double('piece', color: :black) }
-          let(:exp_sqr) do
-            [
-              double('Square', position: { x: 5, y: 3 }, taken?: false, piece: nil),
-              double('Square', position: { x: 6, y: 2 }, taken?: true, piece: blk_piece),
-            ]
-          end
-
-          before do
-            allow(chess_board).to receive(:get_relative_square)
-            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 1, y: -1).and_return(exp_sqr[0])
-            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 2, y: -2).and_return(exp_sqr[1])
-          end
-
-          it 'returns 2 squares { x: 5, y: 3 }, { x: 6, y: 2 }' do
-            result = wht_bishop_44.diagonal_move(chess_board, x, y)
-            expect(result).to match_array(exp_sqr)
-          end
-        end
-
-        context 'when square { x: 5, y: 3 } is taken by :white Piece' do
-          let(:wht_piece) { double('piece', color: :white) }
-          let(:exp_sqr) do
-            [
-              double('Square', position: { x: 5, y: 3 }, taken?: true, piece: wht_piece),
-            ]
-          end
-
-          before do
-            allow(chess_board).to receive(:get_relative_square)
-            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 1, y: -1).and_return(exp_sqr[0])
-          end
-
-          it 'returns empty array' do
-            result = wht_bishop_44.diagonal_move(chess_board, x, y)
-            expect(result).to be_empty
-          end
-        end
-
       end
 
       context 'when given x = -1, y = -1' do
@@ -305,7 +209,6 @@ describe Bishop do
           end
 
           before do
-            allow(chess_board).to receive(:get_relative_square)
             allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: -1, y: -1).and_return(exp_sqr[0])
             allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: -2, y: -2).and_return(exp_sqr[1])
             allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: -3, y: -3).and_return(exp_sqr[2])
@@ -316,42 +219,74 @@ describe Bishop do
             expect(result).to match_array(exp_sqr)
           end
         end
+      end
 
-        context 'when square { x: 2, y: 2 } is taken by :black Piece' do
-          let(:blk_piece) { double('piece', color: :black) }
+      context 'when there is :black Piece on square that Bishop can pass through' do
+        let(:blk_piece) { double('piece', color: :black) }
+        let(:exp_sqr) do
+          [
+            double('Square', position: { x: 5, y: 5 }, taken?: false, piece: nil),
+            double('Square', position: { x: 6, y: 6 }, taken?: false, piece: nil),
+            double('Square', position: { x: 7, y: 7 }, taken?: true, piece: blk_piece)
+          ]
+        end
+
+        before do
+          allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 1, y: 1).and_return(exp_sqr[0])
+          allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 2, y: 2).and_return(exp_sqr[1])
+          allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 3, y: 3).and_return(exp_sqr[2])
+        end
+
+        it 'returns an array of squares passed by Bishop until it encountered :black Piece, including it' do
+          x = 1
+          y = 1
+          expected = exp_sqr
+          result = wht_bishop_44.diagonal_move(chess_board, x, y)
+          expect(result).to match_array(expected)
+        end
+      end
+
+      context 'when there is another :white Piece on square that Bishop can pass through' do
+        let(:wht_piece) { double('piece', color: :white) }
+
+        context 'when Bishop has already passed a few squares' do
           let(:exp_sqr) do
             [
-              double('Square', position: { x: 3, y: 3 }, taken?: false, piece: nil),
-              double('Square', position: { x: 2, y: 2 }, taken?: true, piece: blk_piece),
+              double('Square', position: { x: 5, y: 5 }, taken?: false, piece: nil),
+              double('Square', position: { x: 6, y: 6 }, taken?: false, piece: nil),
+              double('Square', position: { x: 7, y: 7 }, taken?: true, piece: wht_piece)
             ]
           end
 
           before do
-            allow(chess_board).to receive(:get_relative_square)
-            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: -1, y: -1).and_return(exp_sqr[0])
-            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: -2, y: -2).and_return(exp_sqr[1])
+            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 1, y: 1).and_return(exp_sqr[0])
+            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 2, y: 2).and_return(exp_sqr[1])
+            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 3, y: 3).and_return(exp_sqr[2])
           end
 
-          it 'returns 2 squares { x: 3, y: 3 }, { x: 2, y: 2 }' do
+          it 'returns an array of squares passed by Bishop until it encountered another :white Piece, excluding it' do
+            x = 1
+            y = 1
+            expected = exp_sqr.first(2)
             result = wht_bishop_44.diagonal_move(chess_board, x, y)
-            expect(result).to match_array(exp_sqr)
+            expect(result).to match_array(expected)
           end
         end
 
-        context 'when square { x: 3, y: 3 } is taken by :white Piece' do
-          let(:wht_piece) { double('piece', color: :white) }
+        context 'when Bishop hasn\'t made any move yet before encountering that square' do
           let(:exp_sqr) do
             [
-              double('Square', position: { x: 3, y: 3 }, taken?: true, piece: wht_piece),
+              double('Square', position: { x: 5, y: 5 }, taken?: true, piece: wht_piece),
             ]
           end
 
           before do
-            allow(chess_board).to receive(:get_relative_square)
-            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: -1, y: -1).and_return(exp_sqr[0])
+            allow(chess_board).to receive(:get_relative_square).with(start_sqr_44, x: 1, y: 1).and_return(exp_sqr[0])
           end
 
-          it 'returns empty array' do
+          it 'returns an empty array' do
+            x = 1
+            y = 1
             result = wht_bishop_44.diagonal_move(chess_board, x, y)
             expect(result).to be_empty
           end
