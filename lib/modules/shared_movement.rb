89e1bc96e7dd96b7d43d83f22ec893719269020f
Magmusacy <xpfk.098@gmail.com>
2021-05-14 22:34:52 +0200

    Simplify movement method and remove method for discarding related squares

--- lib/modules/shared_movement.rb
+++ lib/modules/shared_movement.rb
@@ -1,32 +1,23 @@
-# This needs refactor btw
 module SharedMovement
-  def next_value(value)
-    value < 0 ? value - 1 : value + 1
-  end
-
-  def reject_related_squares(squares_ary)
-    squares_ary.reject { |sqr| sqr.taken? && sqr.piece.color == color }
-  end
-
   module RookMovement
     include SharedMovement
 
     def horizontal_move(board, x, ary = [])
-      move_square = [board.get_relative_square(location, x: x)].compact
-      return [] if move_square.empty? || reject_related_squares(move_square).empty?
-      return ary << move_square if move_square.first.taken?
+      move_square = board.get_relative_square(location, x: x)
+      return [] if move_square.nil?
       ary << move_square
+      return if move_square.taken?
       horizontal_move(board, next_value(x), ary)
-      reject_related_squares(ary.flatten)
+      ary.flatten
     end
 
     def vertical_move(board, y, ary = [])
-      move_square = [board.get_relative_square(location, y: y)].compact
-      return [] if move_square.empty? || reject_related_squares(move_square).empty?
-      return ary << move_square if move_square.first.taken?
+      move_square = board.get_relative_square(location, y: y)
+      return [] if move_square.nil?
       ary << move_square
+      return if move_square.taken?
       vertical_move(board, next_value(y), ary)
-      reject_related_squares(ary.flatten)
+      ary.flatten
     end
   end
 
@@ -34,12 +25,18 @@ module SharedMovement
     include SharedMovement
 
     def diagonal_move(board, x, y, ary = [])
-      move_square = [board.get_relative_square(location, x: x, y: y)].compact
-      return [] if move_square.empty? || reject_related_squares(move_square).empty?
-      return ary << move_square if move_square.first.taken?
+      move_square = board.get_relative_square(location, x: x, y: y)
+      return [] if move_square.nil?
       ary << move_square
+      return if move_square.taken?
       diagonal_move(board, next_value(x), next_value(y), ary)
-      reject_related_squares(ary.flatten)
+      ary.flatten
     end
+  end
+
+  private
+
+  def next_value(value)
+    value < 0 ? value - 1 : value + 1
   end
 end
