e3a2c048cf6338d1c91883671cc8e3c38e341ed4
Magmusacy <xpfk.098@gmail.com>
2021-05-14 22:01:41 +0200

    Change #legal_moves to #possible_moves and change logic behind removing related squares

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,26 +1,24 @@
 require_relative 'piece'
 
 class King < Piece
-  def legal_moves(board)
+  def possible_moves(board)
     moves = [horizontal_move(board, 1), horizontal_move(board, -1),
              vertical_move(board, 1), vertical_move(board, -1),
              diagonal_move(board, 1, 1), diagonal_move(board, 1, -1),
              diagonal_move(board, -1, 1), diagonal_move(board, -1, -1)].flatten
-    discard_illegal_moves(board, opponent_color, moves)
+
+    reject_related_squares(moves)
   end
 
   def horizontal_move(board, x)
     move_square = [board.get_relative_square(location, x: x)].compact
-    reject_related_squares(move_square)
   end
 
   def vertical_move(board, y)
     move_square = [board.get_relative_square(location, y: y)].compact
-    reject_related_squares(move_square)
   end
 
   def diagonal_move(board, x, y)
-    move_square = [board.get_relative_square(location, x: x, y: y)]
-    reject_related_squares(move_square)
+    move_square = [board.get_relative_square(location, x: x, y: y)].compact
   end
 end
