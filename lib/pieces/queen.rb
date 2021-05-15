6ed9eb08039cf3582f26d88f2a5bdf85c7a46cb4
Magmusacy <xpfk.098@gmail.com>
2021-05-15 17:13:25 +0200

    Add #possible_moves method for Queen

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -1,4 +1,16 @@
 require_relative 'piece'
+require_relative '../modules/shared_movement'
 
 class Queen < Piece
+  include SharedMovement::RookMovement
+  include SharedMovement::BishopMovement
+
+  def possible_moves(board)
+    moves = [horizontal_move(board, 1), horizontal_move(board, -1),
+             vertical_move(board, 1), vertical_move(board, -1),
+             diagonal_move(board, 1, 1), diagonal_move(board, 1, -1),
+             diagonal_move(board, -1, 1), diagonal_move(board, -1, -1)].flatten
+
+    reject_related_squares(moves)
+  end
 end
