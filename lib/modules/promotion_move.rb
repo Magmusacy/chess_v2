018139ddf2057dfc6904c7d9eb8de73f97dfb933
Magmusacy <xpfk.098@gmail.com>
2021-05-18 14:54:16 +0200

    Change how the user is prompted for input

--- lib/modules/promotion_move.rb
+++ lib/modules/promotion_move.rb
@@ -9,17 +9,15 @@ module PromotionMove
   end
 
   def promote(chosen_square, board, piece = nil)
-    piece ||= promotion_input
+    piece ||= piece_input
     self_attributes = [location, color]
     new_piece = create_instance(piece, self_attributes)
     location.update_piece(new_piece)
     new_piece.move(chosen_square, board)
   end
 
-  def promotion_input
+  def piece_input
     loop do
-      show_pieces
-      input = gets.to_i
       verified_piece = verify_piece(input - 1)
       return verified_piece unless verified_piece.nil?
 
@@ -52,12 +50,13 @@ module PromotionMove
     return true if color == :white && y == 7
   end
 
-  def show_pieces
+  def input
     puts 'Type one of the below numbers to pick piece associated with it:'
     puts '1 - Bishop'
     puts '2 - Knight'
     puts '3 - Queen'
     puts '4 - Rook'
+    gets.to_i
   end
 end
 
