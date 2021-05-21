2e6da4c67221ada08dfdc20087fa994d97b8fd02
Magmusacy <xpfk.098@gmail.com>
2021-05-21 16:08:17 +0200

    Create #possible_moves and #knight_move methods for Knight class

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,4 +1,15 @@
 require_relative 'piece'
 
 class Knight < Piece
+  def possible_moves(board)
+    moves = [knight_move(board, 2, 1), knight_move(board, -2, 1), knight_move(board, -2, -1),
+             knight_move(board, 2, -1), knight_move(board, 1, 2), knight_move(board, 1, -2),
+             knight_move(board, -1, 2), knight_move(board, -1, -2)].flatten
+
+    discard_related_squares(moves)
+  end
+
+  def knight_move(board, x, y)
+    board.get_relative_square(location, x: x, y: y)
+  end
 end
