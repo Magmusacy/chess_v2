e4352aba89737fbd8ac68f0ac957aa720e821a0a
Magmusacy <xpfk.098@gmail.com>
2021-05-24 15:20:25 +0200

    Add #move method for King class, and add call to #castling_move in #possible_moves

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,11 +1,15 @@
 require_relative 'piece'
+require_relative '../modules/castling'
 
 class King < Piece
+  include Castling
+
   def possible_moves(board)
     moves = [horizontal_move(board, 1), horizontal_move(board, -1),
              vertical_move(board, 1), vertical_move(board, -1),
              diagonal_move(board, 1, 1), diagonal_move(board, 1, -1),
-             diagonal_move(board, -1, 1), diagonal_move(board, -1, -1)].flatten
+             diagonal_move(board, -1, 1), diagonal_move(board, -1, -1),
+            castling_move(board, 1), castling_move(board, -1)].flatten
 
     discard_related_squares(moves)
   end
@@ -20,5 +24,16 @@ class King < Piece
 
   def diagonal_move(board, x, y)
     move_square = [board.get_relative_square(location, x: x, y: y)].compact
+  end
+
+  def move(chosen_square, board)
+    [1, -1].each do |direction|
+      if chosen_square == castling_move(board, direction)
+        rook_square = board.get_relative_square(location, x: direction)
+        get_rook(board, direction).move(rook_square, board)
+      end
+    end
+
+    super
   end
 end
