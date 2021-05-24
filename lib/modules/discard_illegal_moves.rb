e9dbad2015b74cd22cf8dacc2aa6ed477e3e012c
Magmusacy <xpfk.098@gmail.com>
2021-05-24 17:55:31 +0200

    Simplify the module with additional methods

--- lib/modules/discard_illegal_moves.rb
+++ lib/modules/discard_illegal_moves.rb
@@ -3,24 +3,39 @@ require_relative 'special_conditions'
 module DiscardIllegalMoves
   include SpecialConditions
 
-  def discard_illegal_moves(chess_board, opponent_color, possible_moves)
+  def discard_illegal_moves(chess_board, possible_moves)
     possible_moves.reject do |move_square|
-      board_clone = clone_move(chess_board, self, move_square)
-      illegal?(board_clone, opponent_color)
+      clone_board, clone_piece, clone_square = clone_objects(chess_board, self, move_square)
+      board_clone = clone_board_move(clone_board, clone_piece, clone_square)
+      illegal?(board_clone)
     end
   end
 
-  def illegal?(board_clone, opponent_color)
+  def discard_related_squares(squares)
+    squares.reject { |sqr| sqr.taken? && sqr.piece.color == color }
+  end
+
+  def illegal?(board_clone)
     enemy_pieces = board_clone.squares_taken_by(opponent_color).map(&:piece)
     king_square = board_clone.get_king_square(@color)
     enemy_pieces.any? { |piece| piece.possible_moves(board_clone).include?(king_square) }
   end
 
-  def clone_move(real_board, real_piece, real_square)
+  def clone_objects(real_board, real_piece, real_square)
     clone_board = Marshal.load(Marshal.dump(real_board))
     clone_piece = Marshal.load(Marshal.dump(real_piece))
     clone_square = Marshal.load(Marshal.dump(real_square))
-    clone_piece.move(clone_square, clone_board)
-    clone_board
+    [clone_board, clone_piece, clone_square]
+  end
+
+  def clone_board_move(board, piece, square)
+    piece.move(square, board)
+    board
+  end
+
+  private
+
+  def opponent_color
+    color == :white ? :black : :white
   end
 end
