5e221e7a3c71c8fc2f39b3e7a3bd98c6a233d808
Magmusacy <xpfk.098@gmail.com>
2021-06-05 19:09:30 +0200

    Redo most of the AI logic, add special behaviour logic when moving piece is pawn with a promotion move

--- lib/modules/ai.rb
+++ lib/modules/ai.rb
@@ -1,14 +1,42 @@
 # frozen_string_literal: true
 
+require_relative 'promotion_move'
+
 # Contains logic for picking correct square as an AI
 module AI
-  def ai_pick_square(board)
+  def pick_square(board)
+    sleep 0.15
     possible_squares = board.squares_taken_by(color)
     possible_squares.reject { |sqr| sqr.piece.legal_moves(board).empty? }.sample
   end
 
-  # It might not be needed at all but we'll see
-  def ai_pick_legal_move(legal_moves)
+  def pick_move(legal_moves)
+    sleep 0.15
     legal_moves.sample
+  end
+
+  def ai_move(board)
+    board.display
+    square = pick_square(board)
+    legal_moves = square.piece.legal_moves(board)
+    board.display(square, legal_moves)
+    move = pick_move(legal_moves)
+    if pawn_promotion?(square.piece, board, move)
+      square.piece.promote(move, board, random_new_piece)
+    else
+      square.piece.move(move, board)
+    end
+  end
+
+  def pawn_promotion?(piece, board, square)
+    return unless piece.is_a?(Pawn)
+
+    [piece.promotion_move(board, 1),
+     piece.promotion_move(board, 0),
+     piece.promotion_move(board, -1)].flatten.include?(square)
+  end
+
+  def random_new_piece
+    PromotionMove::PROMOTION_PIECES.sample
   end
 end
