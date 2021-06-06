2bbd7a66057a1620012d1b675e3ef44f179eb0cc
Magmusacy <xpfk.098@gmail.com>
2021-06-06 18:25:04 +0200

    Refactor

--- lib/modules/promotion_move.rb
+++ lib/modules/promotion_move.rb
@@ -1,11 +1,16 @@
+# frozen_string_literal: true
+
 require_relative 'pieces_creator'
 
+# PromotionMove contains logic for promotion move for Pawn class
 module PromotionMove
   include PiecesCreator
+  PROMOTION_PIECES = %i[bishop knight queen rook].freeze
 
-  def promotion_move(board, x)
+  def promotion_move(board, x_shift)
     return [] unless last_move?
-    x.zero? ? vertical_promotion(board) : diagonal_promotion(board, x)
+
+    x_shift.zero? ? vertical_promotion(board) : diagonal_promotion(board, x_shift)
   end
 
   def promote(chosen_square, board, piece = nil)
@@ -27,27 +32,29 @@ module PromotionMove
 
   def verify_piece(input)
     return unless [0, 1, 2, 3].include?(input)
-    [:bishop, :knight, :queen, :rook][input]
+
+    PROMOTION_PIECES[input]
   end
 
   private
 
   def vertical_promotion(board)
-    move = [board.get_relative_square(location, y: y_shift)]
-    return move unless move.first.taken?
-    []
+    move = [board.get_relative_square(location, y: y_shift)].compact
+    return [] if move.empty? || move.first.taken?
+
+    move
   end
 
-  def diagonal_promotion(board, x)
-    move = [board.get_relative_square(location, x: x, y: y_shift)]
-    return [] unless move.first.taken?
+  def diagonal_promotion(board, x_shift)
+    move = [board.get_relative_square(location, x: x_shift, y: y_shift)].compact
+    return [] if move.empty? || !move.first.taken?
+
     discard_related_squares(move)
   end
 
   def last_move?
     y = location.position[:y]
-    return true if color == :black && y == 2
-    return true if color == :white && y == 7
+    true if (color == :black && y == 2) || (color == :white && y == 7)
   end
 
   def input
@@ -59,4 +66,3 @@ module PromotionMove
     gets.to_i
   end
 end
-
